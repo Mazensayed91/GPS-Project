b6dbd6ceb331d8344d5c2b06d8c505c27824f037
Mazensayed91 <sayedmazen70@gmail.com>
2021-06-08 22:12:35 +0200

    Modified GPIO initialization to a using a new libarary

--- GPIO.c
+++ GPIO.c
@@ -4,62 +4,46 @@
 #include "TM4C123GH6PM2.h"
 
 
-void PortA_Init() {  //command port (PA5 for RS ,PA6 for R/W, PA7 for E)
-	
-	SYSCTL_RCGCGPIO_R |= 0x01;     // 1) A clock
-	while ((SYSCTL_PRGPIO_R & 0x01) == 0) {};  
-	//     LOCK 
-	GPIO_PORTA_CR_R = 0xE0;           // allow changes to PA7-5       
-	GPIO_PORTA_AMSEL_R = 0x00;        // 3) disable analog function
-	GPIO_PORTA_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
-	GPIO_PORTA_DIR_R = 0xE0;          // 5) PB7-PA5 output   
-	GPIO_PORTA_AFSEL_R = 0x00;        // 6) no alternate function
-	GPIO_PORTA_PUR_R = 0x00;          // disable pullup resistors         
-	GPIO_PORTA_DEN_R = 0xE0;          // 7) enable digital pins PA7-PA0        
+void initA()
+{ //command port (PA5 for RS ,PA6 for R/W, PA7 for E)
+    SYSCTL->RCGCGPIO |= GPIO_PIN_1;
+    while ((SYSCTL->PRGPIO & GPIO_PIN_1)!= GPIO_PIN_1){};
+    GPIOA->AFSEL |= GPIO_PIN_10;
+    GPIOA->PCTL = (GPIOA->PCTL & 0xFFFFFF00) | (GPIO_PCTL_PA0_U0RX | GPIO_PCTL_PA1_U0TX);
+    GPIOA->DEN |= GPIO_PIN_10; // enable digital enable
 }
 
-void PortB_Init(){    // Data port 
-  
-  SYSCTL->RCGCGPIO |= 0x02; // Clock EN to GPIOB 
+void PortB_Init(){    // Data port
+
+  SYSCTL->RCGCGPIO |= 0x02; // Clock EN to GPIOB
 	GPIOB->DIR = 0xFF; // Set PORTB pins as output
 	GPIOB->DEN = 0xFF; // Set PORTB pins as Digital
 }
 
-void PortD_Init()  // GPS port
-{
-
-	SYSCTL_RCGCGPIO_R |= 0x08;
-	while ((SYSCTL_PRGPIO_R & 0x08) == 0) {};
-	GPIO_PORTD_LOCK_R = 0x4C4F434B;
-	GPIO_PORTD_CR_R = 0xFF;
-	GPIO_PORTD_AFSEL_R = 0;
-	GPIO_PORTD_PCTL_R = 0;
-	GPIO_PORTD_AMSEL_R = 0;	
-	GPIO_PORTD_PUR_R = 0x00;	// need to add
-	GPIO_PORTD_DIR_R = 0x00;  // need to add
-	GPIO_PORTD_DEN_R = 0xFF;
-	
-}
-void PortF_Init()  // led & switches port
-{
-	
-	 SYSCTL_RCGCGPIO_R |= 0x20;
-    while((SYSCTL_PRGPIO_R & 0x20) != 0x20){};
-    GPIO_PORTF_LOCK_R = 0x4C4F434B;
-    GPIO_PORTF_CR_R = 0X1F;
-    GPIO_PORTF_AFSEL_R = 0;
-    GPIO_PORTF_PCTL_R = 0;
-    GPIO_PORTF_AMSEL_R = 0;
-    GPIO_PORTF_DEN_R = 0x1F;
-    GPIO_PORTF_PUR_R = 0x11;
-    GPIO_PORTF_DIR_R = 0x0E;
+void initD(){
+    SYSCTL->RCGCGPIO |= 0x08;
+    while ((SYSCTL->PRGPIO & 0x08)!= 0x08){};
+    GPIOD->AFSEL |= 0xC0;
+    GPIOD->PCTL = (GPIOD->PCTL & 0x00FFFFFF) | (0x10000000 | 0x01000000);
+    GPIOD->DEN |= 0xC0;
 }
 
-void distance(uint32_t dis)
+void initF() // Led and switches ports
 {
-	if (dis >= 100) {
+    SYSCTL->RCGCGPIO |= 0x20;
+    while ((SYSCTL->PRGPIO & 0x20)!= 0x20){};
+    GPIOF->AFSEL = 0;
+    GPIOF->PCTL = 0;
+    GPIOF->AMSEL = 0;
+    GPIOF->PUR = 0x11;
+    GPIOF->DIR = 0x0E;
+    GPIOF->DEN = 0x1F;
+}
 
-		GPIO_PORTF_DATA_R |= (1 << 1);
+void distance_100(uint32_t dis)
+{
+    if (dis >= 100) {
 
-	}
+        GPIOF->DATA |= (1 << 1);
+    }
 }
